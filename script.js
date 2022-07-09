28d25bf93946650e412f0efe392b16c62bd9caa3
greg@gregPredator <greg19850@gmail.com>
2022-07-09 08:50:57 +0100

    created function changing background color

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ let blue = 100;
 document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
 
 
-const changeColor = function () { }
+const changeColor = function (event) {
+    if (event.keyCode === 38 && red < 255) {
+        red += 5;
+        green += 5;
+        blue += 5;
+    } else if (event.keyCode === 40 && red > 0) {
+        red -= 5;
+        green -= 5;
+        blue -= 5;
+    }
+    document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
+}
 
-document.body.addEventListener('arrowdown', changeColor)
+document.body.addEventListener('keydown', changeColor)
