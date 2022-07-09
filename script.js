25b4a3ea5808be11c174e364a9faee948a3d4c26
greg@gregPredator <greg19850@gmail.com>
2022-07-09 07:36:21 +0100

    created eventListener for body, and assigned function to it

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let green = 100;
 let blue = 100;
 
 document.body.style.backgroundColor = `rgb(${red},${green},${blue})`
+
+
+const changeColor = function () { }
+
+document.body.addEventListener('arrowdown', changeColor)
